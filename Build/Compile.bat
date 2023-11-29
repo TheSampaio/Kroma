45140bfe27d818c019fd05b1a4398ee464633568
Kellvyn Sampaio <64695440+TheSampaio@users.noreply.github.com>
2023-11-29 13:25:54 -0300

    Update Compile.bat
    • Build system improved.

--- Build/Compile.bat
+++ Build/Compile.bat
@@ -7,6 +7,6 @@ CLS
 CD ../
 SET /P NAME="Type the project's name: "
 ECHO ===
-pyinstaller Source/main.py --onefile --noconsole --noconfirm --name "%NAME%" --distpath "_Output/Bin/%NAME%" --workpath "_Output/Obj/" --specpath "_Output/Obj/%NAME%"
+pyinstaller Source/main.py --onedir --noconsole --noconfirm --name "%NAME%" --icon "../../../Data/Icon/icon-group-retiro.ico" --distpath "_Output/Bin/" --workpath "_Output/Obj/" --specpath "_Output/Obj/%NAME%"
 ECHO ===
 PAUSE
